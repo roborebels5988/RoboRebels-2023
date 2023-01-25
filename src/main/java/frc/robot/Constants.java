8ce427f25c46866ed42edf3c72d4b3eb73082279
AnnoyingRain5 <annoyingrain5@gmail.com>
2023-01-25 12:54:52 +1100

    Add constant ports for motors and controllers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,15 +5,27 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    // controller ports
+    public static final short kDriverControllerPort = 0;
+    public static final short kSubsystemControllerPort = 1;
+
+    // motor ports
+    public static final short leftFrontMotorPort = 0;
+    public static final short leftBackMotorPort = 1;
+    public static final short rightFrontMotorPort = 2;
+    public static final short rightBackMotorPort = 3;
   }
 }
