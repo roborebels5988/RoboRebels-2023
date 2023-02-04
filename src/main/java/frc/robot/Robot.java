f1fb9368f87935e5f20ed6ad1e650ab7674786ea
AnnoyingRain5 <annoyingrain5@gmail.com>
2023-02-05 10:58:28 +1100

    schedule autocommand properly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.ControllerDriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -16,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private Command m_ControllerDriveTrain;
 
   private RobotContainer m_robotContainer;
 
@@ -77,6 +79,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    m_ControllerDriveTrain = new ControllerDriveTrain();
+    m_ControllerDriveTrain.schedule();
   }
 
   /** This function is called periodically during operator control. */
