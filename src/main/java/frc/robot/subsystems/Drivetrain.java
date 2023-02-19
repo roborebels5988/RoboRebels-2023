fef2fe614dfdbfa96b963e8973d94263aa7e22f0
AnnoyingRains <annoyingrain5@gmail.com>
2023-02-19 12:26:34 +1100

    Readded encoder code and init command (oops)

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -16,13 +15,13 @@ public class Drivetrain extends SubsystemBase {
         public Drivetrain() {
         }
 
-        private static final MotorController leftFrontPWMVictorSPX = new PWMVictorSPX(
+        private static final MotorController leftFrontPWMVictorSPX = new WPI_VictorSPX(
                         Constants.MotorConstants.leftFrontMotorPort);
-        private static final MotorController rightFrontPWMVictorSPX = new PWMVictorSPX(
+        private static final MotorController rightFrontPWMVictorSPX = new WPI_VictorSPX(
                         Constants.MotorConstants.rightFrontMotorPort);
-        private static final MotorController leftBackPWMVictorSPX = new PWMVictorSPX(
+        private static final MotorController leftBackPWMVictorSPX = new WPI_VictorSPX(
                         Constants.MotorConstants.leftBackMotorPort);
-        private static final MotorController rightBackPWMVictorSPX = new PWMVictorSPX(
+        private static final MotorController rightBackPWMVictorSPX = new WPI_VictorSPX(
                         Constants.MotorConstants.rightBackMotorPort);
 
         private static final MotorControllerGroup left = new MotorControllerGroup(leftFrontPWMVictorSPX,
@@ -32,11 +31,18 @@ public class Drivetrain extends SubsystemBase {
 
         public static final DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);
 
-        public CommandBase MethodCommand() {
+        public static final Encoder LeftEncoder = new Encoder(Constants.EncoderConstants.leftEncoderPortA,
+                        Constants.EncoderConstants.leftEncoderPortB);
+        public static final Encoder RightEncoder = new Encoder(Constants.EncoderConstants.rightEncoderPortA,
+                        Constants.EncoderConstants.rightEncoderPortB);
+
+        public CommandBase Init() {
 
                 return runOnce(
                                 () -> {
-
+                                        // distance per pulse = pi * wheel hub diameter / pulses per revolution
+                                        RightEncoder.setDistancePerPulse(Math.PI * 15.24 / 5);
+                                        LeftEncoder.setDistancePerPulse(Math.PI * 15.24 / 5);
                                 });
         }
 
