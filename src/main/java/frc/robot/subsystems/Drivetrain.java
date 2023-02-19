cca41773dfe684690512caced0b3f10379fbd021
AnnoyingRains <annoyingrain5@gmail.com>
2023-02-19 11:44:25 +1100

    switched drivetrain to use CAN, added pheonix lib

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,6 +1,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -11,44 +12,44 @@ import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
 
-  public Drivetrain() {
-  }
+    public Drivetrain() {
+    }
 
-  private static final MotorController leftFrontPWMVictorSPX = new PWMVictorSPX(
-      Constants.MotorConstants.leftFrontMotorPort);
-  private static final MotorController rightFrontPWMVictorSPX = new PWMVictorSPX(
-      Constants.MotorConstants.rightFrontMotorPort);
-  private static final MotorController leftBackPWMVictorSPX = new PWMVictorSPX(
-      Constants.MotorConstants.leftBackMotorPort);
-  private static final MotorController rightBackPWMVictorSPX = new PWMVictorSPX(
-      Constants.MotorConstants.rightBackMotorPort);
+    private static final WPI_VictorSPX leftFrontPWMVictorSPX = new WPI_VictorSPX(
+            Constants.MotorConstants.leftFrontMotorPort);
+    private static final WPI_VictorSPX rightFrontPWMVictorSPX = new WPI_VictorSPX(
+            Constants.MotorConstants.rightFrontMotorPort);
+    private static final WPI_VictorSPX leftBackPWMVictorSPX = new WPI_VictorSPX(
+            Constants.MotorConstants.leftBackMotorPort);
+    private static final WPI_VictorSPX rightBackPWMVictorSPX = new WPI_VictorSPX(
+            Constants.MotorConstants.rightBackMotorPort);
 
-  private static final MotorControllerGroup left = new MotorControllerGroup(leftFrontPWMVictorSPX,
-      leftBackPWMVictorSPX);
-  private static final MotorControllerGroup right = new MotorControllerGroup(rightFrontPWMVictorSPX,
-      rightBackPWMVictorSPX);
+    private static final MotorControllerGroup left = new MotorControllerGroup(leftFrontPWMVictorSPX,
+            leftBackPWMVictorSPX);
+    private static final MotorControllerGroup right = new MotorControllerGroup(rightFrontPWMVictorSPX,
+            rightBackPWMVictorSPX);
 
-  public static final DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);
+    public static final DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);
 
-  public CommandBase MethodCommand() {
+    public CommandBase MethodCommand() {
 
-    return runOnce(
-        () -> {
+        return runOnce(
+                () -> {
 
-        });
-  }
+                });
+    }
 
-  public boolean Condition() {
+    public boolean Condition() {
 
-    return false;
-  }
+        return false;
+    }
 
-  @Override
-  public void periodic() {
-  }
+    @Override
+    public void periodic() {
+    }
 
-  @Override
-  public void simulationPeriodic() {
+    @Override
+    public void simulationPeriodic() {
 
-  }
+    }
 }
