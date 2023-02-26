8ec3d81e992bc5b7fac08f03bb30d82b6016a13e
AnnoyingRains <annoyingrain5@gmail.com>
2023-02-26 13:54:47 +1100

    Added shuffleboard info

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,12 +3,15 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -39,14 +42,22 @@ public class Drivetrain extends SubsystemBase {
         public static final Encoder RightEncoder = new Encoder(Constants.EncoderConstants.rightEncoderPortA,
                         Constants.EncoderConstants.rightEncoderPortB);
 
-        public static final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+        private static GenericEntry speed;
+
+        public static final ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
 
         public static void Init() {
                 // distance per pulse = pi * wheel hub diameter / pulses per revolution
                 RightEncoder.setDistancePerPulse(Math.PI * 15.24 / 5);
                 LeftEncoder.setDistancePerPulse(Math.PI * 15.24 / 5);
+                LeftEncoder.setReverseDirection(true);
                 gyro.calibrate();
-                Shuffleboard.getTab("Main").add(gyro);
+                ShuffleboardTab MainTab = Shuffleboard.getTab("Main");
+                MainTab.add("Gyro", gyro);
+                MainTab.add("Left Encoder", LeftEncoder);
+                MainTab.add("Right Encoder", RightEncoder);
+                speed = MainTab.add("kmph", 0).getEntry();
+                MainTab.add(m_robotDrive);
         };
 
         public boolean Condition() {
@@ -56,7 +67,9 @@ public class Drivetrain extends SubsystemBase {
 
         @Override
         public void periodic() {
-
+                Double AvgSpeedPerSecond = (LeftEncoder.getRate() + RightEncoder.getRate()) / 2;
+                double RoundAvgSpeedPerHour = Math.round(AvgSpeedPerSecond / 27.778);
+                speed.setDouble(RoundAvgSpeedPerHour / 10);
         }
 
         @Override
