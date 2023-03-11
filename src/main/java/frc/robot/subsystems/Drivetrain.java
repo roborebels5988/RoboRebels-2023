313bec60cbbe8bc316eb0d2db7d47c3d6a495c8f
AnnoyingRains <annoyingrain5@gmail.com>
2023-03-11 18:10:31 +1100

    latest attempt

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -71,7 +71,7 @@ public class Drivetrain extends SubsystemBase {
                 Double AvgSpeedPerSecond = (LeftEncoder.getRate() + RightEncoder.getRate()) / 2;
                 double RoundAvgSpeedPerHour = Math.round(AvgSpeedPerSecond / 27.778);
                 speed.setDouble(RoundAvgSpeedPerHour / 10);
-                dashgyro.setDouble(dashgyro.getDouble(RoundAvgSpeedPerHour) + 1);
+                dashgyro.setFloat(gyro.getFusedHeading());
         }
 
         @Override
